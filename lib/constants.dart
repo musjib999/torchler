5e86acd35696a3098d2fd987d1332b350fbfc2a3
musa <musjib999@gmail.com>
2020-05-23 14:09:12 +0100

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const kOffButtonTextStyle = TextStyle(
+  fontSize: 35.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kOnButtonTextStyle = TextStyle(
+  fontSize: 35.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kActiveButton = Color(0xffFEA200);
+const kInactiveButton = Color(0xFFF2F2F2);
+
+const kActiveBackgroundColor = Color(0xff21314e);
+const kInactiveBackgroundColor = Colors.white;
+
+const kActiveTextColor = Colors.white;
+const kInactiveTextColor = Color(0xFF999999);
+
+const kIconOnColor = Color(0xFFFEA200);
+const kIconOffColor = Colors.black;
---
+++ lib/torch_light.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flashlight/flashlight.dart';
+
+int brightness = 50;
+
+enum Button{
+  onButton,
+  offButton
+}
+Button selectedButton = Button.offButton;
+
+class TorchlerPage extends StatefulWidget {
+  @override
+  _TorchlerPageState createState() => _TorchlerPageState();
+}
+
+class _TorchlerPageState extends State<TorchlerPage> {
+  bool _hasFlashlight = false;
+  
+  @override
+  initState() {
+    super.initState();
+    initFlashlight();
+  }
+
+  initFlashlight() async{
+    bool hasFlash = await Flashlight.hasFlashlight;
+    setState(() {
+      _hasFlashlight = hasFlash;
+    });
+  }
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: selectedButton == Button.onButton ? kActiveBackgroundColor : kInactiveBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Center(
+                child: Icon(
+                  FontAwesomeIcons.lightbulb,
+                  size: 200.0,
+                  color: selectedButton == Button.onButton ? kIconOnColor : kIconOffColor,
+                ),
+              ),
+            ),
+            SizedBox(height: 130.0,),
+            Center(
+              child: Text(
+                brightness.toString(),
+                style: TextStyle(
+                  color: selectedButton == Button.onButton ? Colors.white : Colors.black,
+                ),
+              ),
+            ),
+            SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                activeTrackColor: kActiveButton,
+                inactiveTrackColor: kInactiveButton,
+                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 13.0),
+                thumbColor: kActiveButton,
+              ),
+              child: Slider(
+                onChanged: (newValue){
+                  setState(() {
+                    brightness = newValue.round();
+                  });
+                },
+                value: brightness.toDouble(),
+                min: 0,
+                max: 100,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(40.0, 20.0, 0, 0),
+                    padding: EdgeInsets.all(20.0),
+                    width: 100.0,
+                    height: 75.0,
+                    decoration: BoxDecoration(
+                      color: selectedButton == Button.offButton ? kActiveButton : kInactiveButton,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        bottomLeft: Radius.circular(20.0),
+                      ),
+                    ),
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedButton = Button.offButton;
+                          });
+                          Flashlight.lightOff();
+                        },
+                        child: Text(
+                          'OFF',
+                          style: kOffButtonTextStyle.apply(
+                            color : selectedButton == Button.offButton ? kActiveTextColor : kInactiveTextColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 20.0, 40.0, 0),
+                    padding: EdgeInsets.all(20.0),
+                    width: 100.0,
+                    height: 75.0,
+                    decoration: BoxDecoration(
+                      color: selectedButton == Button.onButton ? kActiveButton : kInactiveButton,
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0)),
+                    ),
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            selectedButton = Button.onButton;
+                          });
+                          Flashlight.lightOn();
+                        },
+                        child: Text(
+                          'ON',
+                          style: kOnButtonTextStyle.apply(
+                            color : selectedButton == Button.onButton ? kActiveTextColor : kInactiveTextColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
